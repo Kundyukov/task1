f255ec8d1640d41305da3555775a1c2c1feceb8f
Kundyukov <116679446+Kundyukov@users.noreply.github.com>
2022-11-12 17:59:33 +0300

    delete useless "if"

--- tabulation.cpp
+++ tabulation.cpp
@@ -8,45 +8,21 @@ double Tabul(double leftval, double rightval, double step)
     double y;
     double x1;
     double y1;
-    int check = 0;
     for (double x = leftval; x <= rightval; x += step)
     {
-        y = sqrt(x * x - 4 * log(x)); //2 Mine
-        //y = x / (x + 1); //2 
-        //y = 2 * ((abs(x + 3)) / (x + 3)) * x + 6;//neustr  1 
-        //y = sin(x) / x;//1 
-        //y = sqrt(x * x + 12 - log(x * x));//other test 
+        y = sqrt(x * x - 4 * log(x));
             {
             std::cout << "x: " << x << " y: " << y << std::endl;
-            if (y >= INFINITY)//discontinuity
+            if (y >= INFINITY)//discontinuity test
             {
-                check = 3;
                 x1 = x;
                 y1 = y;
             }
            
         }
-        if (y <= ( - (INFINITY))) 
-        {
-            check = 2;
-            x1 = x;
-            y1 = y;
-        }
-        if(isnan(y))// nan check
-        {
-            check = 1;
-            x1 = x;
-            y1 = y;
-        }
     }
 
-    std::cout << "\nresult:\n";
-    if (check == 1)
-        std::cout << "x: " << x1 << " y: " << y1 << " - nan, discontinuity!\n";
-    if (check == 2)
-        std::cout << "x: " << x1 << " y: " << y1 << " - infinite\n";
-    if (check == 3)
-        std::cout << "x: " << x1 << " y: " << y1 << " - infinite discontinuity!\n";
+    std::cout << "\nresult:\n" << "x: " << x1 << " y: " << y1 << " - infinite discontinuity!\n";
 
     return y;
 }
